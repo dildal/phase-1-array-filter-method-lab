28c493a2e4bb6032518fa8b621ea26dcc3c972ad
Michael Dally <mddally@gmail.com>
2022-02-02 13:45:25 -0500

    commit tree aafb31cb417eab4c34c2c8861a0ca04d507d389e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter((driver)=> {
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(names, chars){
+    return names.filter((name) => {
+        return name.indexOf(chars) === 0;
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter((driver) => {
+        return driver.name === name;
+    })
+}
